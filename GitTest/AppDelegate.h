cc6c6e8d262b83cc08f6d939eddd9349906dedf7
chm510 <hyemin.choi51@gmail.com>
2015-02-09 14:17:27 +0900

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by CHOIHYEMIN on 2015. 2. 9..
+//  Copyright (c) 2015년 Laidback Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitTest
+//
+//  Created by CHOIHYEMIN on 2015. 2. 9..
+//  Copyright (c) 2015년 Laidback Co., Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
