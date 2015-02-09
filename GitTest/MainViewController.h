6754806ff9e49e730869cdb05d28d14218b0568d
chm510 <hyemin.choi51@gmail.com>
2015-02-09 14:39:29 +0900

    테스트 커밋

---
+++ GitTest/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  GitTest
+//
+//  Created by CHOIHYEMIN on 2015. 2. 9..
+//  Copyright (c) 2015년 Laidback Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ GitTest/MainViewController.m
@@ -0,0 +1,37 @@
+//
+//  MainViewController.m
+//  GitTest
+//
+//  Created by CHOIHYEMIN on 2015. 2. 9..
+//  Copyright (c) 2015년 Laidback Co., Ltd. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
